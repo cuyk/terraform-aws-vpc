0dc65234e7ad20b0b7e6b40e2fda1bbcb7f96078
Vova Pochtar <v.pochtar@gmail.com>
2022-08-19 16:50:09 +0300

    fix: Create public subnets if 'var.one_nat_gateway_per_az == true' and 'length(var.public_subnets) >= length(var.azs)'

--- main.tf
+++ main.tf
@@ -350,7 +350,7 @@ resource "aws_route_table" "intra" {
 ################################################################################
 
 resource "aws_subnet" "public" {
-  count = local.create_vpc && length(var.public_subnets) > 0 && (false == var.one_nat_gateway_per_az || length(var.public_subnets) >= length(var.azs)) ? length(var.public_subnets) : 0
+  count = local.create_vpc ? length(var.public_subnets) : 0
 
   vpc_id                          = local.vpc_id
   cidr_block                      = element(concat(var.public_subnets, [""]), count.index)
